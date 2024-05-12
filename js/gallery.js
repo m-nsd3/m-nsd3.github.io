feadf3411715c18164d27e47bec2dd3d390fe0db
m-nsd3 <coutustom3@outlook.com>
2024-05-12 11:29:39 +0900

    11.4まで済

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,30 @@
+// 画像ギャラリー機能を動かす
+// ここでやりたいのは、ギャラリーの各サムネイル画像にイベントリスナーをアタッチして
+// クリックされたときにメイン画像をサムネイル画像に対応するものに差し替えること
 function activateGallery() {
-    alert("ギャラリーファイルからこんにちは！");
-  }
+  let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage = document.querySelector("#gallery-photo img");
+
+  // 更新される画像情報
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // クリックされたサムネイル画像をメイン画像として設定する
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // 現在の画像を変更する
+      document.querySelector(".current").classList.remove("current");
+      thumbnail.parentNode.classList.add("current");
+
+      // 画像の情報を更新する
+    title.innerHTML       = thumbnail.dataset.title;
+    description.innerHTML = thumbnail.dataset.description;
+    });
+  });
+}
   
